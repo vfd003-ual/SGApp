9c0a6ad9af46ef4c6612085e32a8dead13c3184d
Manuel Torres Gil <mtorres@ual.es>
2020-11-01 17:33:52 +0100

    Uso de variables de entorno

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
       <tbody>
         <?php
 
-        $conexion = mysqli_connect("mysql", "root", "secret", "SG");
+        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "SG");
 
         $cadenaSQL = "select * from s_customer";
         $resultado = mysqli_query($conexion, $cadenaSQL);
